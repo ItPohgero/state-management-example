8512f0aad79269c8c29e5fdfcf4c4891862c86a6
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-06-02 09:28:53 +0700

    context

---
+++ src/state/context/usage.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { CounterProvider, useCounter } from "./setup";
+
+const CounterComponent = () => {
+    const { count, increment, decrement } = useCounter();
+    return (
+        <div className="bg-neutral-50 w-full p-4 rounded-lg">
+            <p className="text-center">Count: {count}</p>
+            <div className="flex justify-between mt-4">
+                <button type='button' onClick={increment}>In</button>
+                <button type='button' onClick={decrement}>De</button>
+            </div>
+        </div>
+    );
+};
+
+export default function UsageContext() {
+    return (
+        <CounterProvider>
+            <CounterComponent />
+        </CounterProvider>
+    );
+}
